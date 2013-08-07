d5c4ed0b7cc03a85a0ace91ae93275771610b0d5
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-08-07 20:37:40 +0900

    Added routes which are view_design, settings

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,6 @@ Minicms::Application.routes.draw do
    
   #basic organization informations
   resources :organizations, :except => [:destroy]
-  # # # # # # # # # # # 
-  #  organizations GET    /organizations(.:format)          organizations#index
-  #                POST   /organizations(.:format)          organizations#create
-  #  new_organization GET    /organizations/new(.:format)      organizations#new
-  #  edit_organization GET    /organizations/:id/edit(.:format) organizations#edit
-  #  organization GET    /organizations/:id(.:format)      organizations#show
-  #                PUT    /organizations/:id(.:format)      organizations#update
-  # # # # # # # # # # #
   
   #detail organization informations
   match 'organizations/:id/informations/new', :to => "organization_informations#new", :via => :get
@@ -17,11 +9,19 @@ Minicms::Application.routes.draw do
   match 'organizations/:id/informations', :to => "organization_informations#show", :via => :get
   match 'organizations/:id/informations', :to => "organization_informations#create", :via => :post
   match 'organizations/:id/informations', :to => "organization_informations#update", :via => :put
-  # # # # # # # # # # #
-  #  GET  /organizations/:id/informations/new(.:format) organization_informations#new
-  #  GET  /organizations/:id/informations(.:format)     organization_informations#show
-  #  POST /organizations/:id/informations(.:format)     organization_informations#create
-  #  PUT  /organizations/:id/informations(.:format)      organization_informations#update
-  # # # # # # # # # # #
+
+  #view design of each organization
+  match 'organizations/:id/view_design/new', :to => "view_design#new", :via => :get
+  match 'organizations/:id/view_design/edit', :to => "view_design#edit", :via => :get
+  match 'organizations/:id/view_design', :to => "view_design#show", :via => :get
+  match 'organizations/:id/view_design', :to => "view_design#create", :via => :post
+  match 'organizations/:id/view_design', :to => "view_design#update", :via => :put
+ 
+  #settings of ehach organization homepage
+  match 'organizations/:id/settings/new', :to => "settings#new", :via => :get
+  match 'organizations/:id/settings/edit', :to => "settings#edit", :via => :get
+  match 'organizations/:id/settings', :to => "settings#show", :via => :get
+  match 'organizations/:id/settings', :to => "settings#create", :via => :post
+  match 'organizations/:id/settings', :to => "settings#update", :via => :put
 
 end
