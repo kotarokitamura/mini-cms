a332b7744f1f26e0eeb424d196ee709b4133ba9a
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-08-07 20:11:47 +0900

    Added routes of organization_information

--- config/routes.rb
+++ config/routes.rb
@@ -1,62 +1,27 @@
 Minicms::Application.routes.draw do
    
-  resources :organizations
+  #basic organization informations
+  resources :organizations, :except => [:destroy]
+  # # # # # # # # # # # 
+  #  organizations GET    /organizations(.:format)          organizations#index
+  #                POST   /organizations(.:format)          organizations#create
+  #  new_organization GET    /organizations/new(.:format)      organizations#new
+  #  edit_organization GET    /organizations/:id/edit(.:format) organizations#edit
+  #  organization GET    /organizations/:id(.:format)      organizations#show
+  #                PUT    /organizations/:id(.:format)      organizations#update
+  # # # # # # # # # # #
   
+  #detail organization informations
+  match 'organizations/:id/informations/new', :to => "organization_informations#new", :via => :get
+  match 'organizations/:id/informations/edit', :to => "organization_informations#edit", :via => :get
+  match 'organizations/:id/informations', :to => "organization_informations#show", :via => :get
+  match 'organizations/:id/informations', :to => "organization_informations#create", :via => :post
+  match 'organizations/:id/informations', :to => "organization_informations#update", :via => :put
+  # # # # # # # # # # #
+  #  GET  /organizations/:id/informations/new(.:format) organization_informations#new
+  #  GET  /organizations/:id/informations(.:format)     organization_informations#show
+  #  POST /organizations/:id/informations(.:format)     organization_informations#create
+  #  PUT  /organizations/:id/informations(.:format)      organization_informations#update
+  # # # # # # # # # # #
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
