089733e95654c50349233cd3d75e1ed6bebd69c0
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-08-07 17:18:18 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minicms::Application.config.secret_token = '93e3fb0255eeab6695bd50fabe9379bd36102cd5489c1f20575a8ccbed461c616878b0b2b8d455f23a85be4aa535c428dbbc14a6d7cf3c4c017040df30dbb0e6'
