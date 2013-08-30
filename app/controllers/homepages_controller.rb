1a91f13cdfa65853d1750d609a9eee6011b1b701
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-08-30 17:05:45 +0900

    Change root site and remove index.html

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,6 +1,14 @@
 class HomepagesController < ApplicationController
- def show 
+ def preview 
    @organization = Organization.find(params[:id])
+   @organization_infos = @organization.organization_infos
+   @stores = @organization.stores
+   render :template => "homepages/show2"
+ end
+  
+ def show 
+   #@organization = Organization.find(params[:id])
+   @organization = Organization.find(1)
    @organization_infos = @organization.organization_infos
    @stores = @organization.stores
    render :template => "homepages/show2"
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Minicms::Application.routes.draw do
   match "organizations/:organization_id/organization_infos", :via => :get, :to => "organization_infos#edit", :as => "edit_organization_infos" 
   match "organizations/:organization_id/organization_infos", :via => :put, :to => "organization_infos#update", :as => "organization_infos" 
 
-  match "organizations/:id/homepage", :via => :get, :to => "homepages#show"
+  match "organizations/:id/homepage", :via => :get, :to => "homepages#preview"
+  
+  root :to => "homepages#show"
 end
