38851db7dbc469273ba911cf55400eb4c3731841
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-08-30 17:15:01 +0900

    Added get subdomain module but not yet finish

---
+++ lib/assets/subdomain.rb
@@ -0,0 +1,20 @@
+module Subdomain
+
+  def self.included(base)
+    base.extend ClassMethod
+  end
+
+  module ClassMethod
+    def get_url
+  
+    end
+  
+    def get_subdomain
+
+    end
+
+    def get_organization_id
+  
+    end
+  end
+end
