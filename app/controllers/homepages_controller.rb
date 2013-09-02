d81c123dbfa2f21a6b15cf07cff9526e6f88ac17
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-09-02 11:50:46 +0900

    Add function which find organizaiton_id from subdomain

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -3,14 +3,12 @@ class HomepagesController < ApplicationController
    @organization = Organization.find(params[:id])
    @organization_infos = @organization.organization_infos
    @stores = @organization.stores
-   render :template => "homepages/show2"
+   render :template => "homepages/show"
  end
   
  def show 
-   #@organization = Organization.find(params[:id])
-   @organization = Organization.find(1)
-   @organization_infos = @organization.organization_infos
-   @stores = @organization.stores
+   organization_id = Homepage.get_organization_id_by_subdomain(request.url)
+   @organization = Organization.find(organization_id)
    render :template => "homepages/show2"
  end
 end
--- lib/assets/subdomain.rb
+++ lib/assets/subdomain.rb
@@ -1,20 +1,24 @@
 module Subdomain
-
   def self.included(base)
-    base.extend ClassMethod
+    base.extend ClassMethods
   end
-
-  module ClassMethod
-    def get_url
-  
-    end
-  
-    def get_subdomain
-
+ 
+  module ClassMethods
+    def get_organization_id_by_subdomain(url)
+      organization_code = get_organization_code(url)
+      get_organization_id(organization_code)      
     end
 
-    def get_organization_id
-  
+    def get_organization_code(url)
+      #get after "://"
+      url =~ /:\/\//
+      #get before ".kitasang.com"
+      $'=~ /\./
+      $` 
+    end
+
+    def get_organization_id(organization_code)
+      Organization.find_by_organization_code(organization_code).id
     end
   end
 end
