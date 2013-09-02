be8b26ce240a4016cd57098a325815e32ecec4b4
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-09-02 14:20:31 +0900

    Add validation in Subdomain module

--- lib/assets/subdomain.rb
+++ lib/assets/subdomain.rb
@@ -1,24 +1,38 @@
 module Subdomain
+  DOMAIN = "kitasang.com"
   def self.included(base)
     base.extend ClassMethods
   end
  
   module ClassMethods
     def get_organization_id_by_subdomain(url)
-      organization_code = get_organization_code(url)
-      get_organization_id(organization_code)      
+      if url_valid?(url)
+        subdomain = get_organization_code(url)
+        if subdomain_valid?(subdomain)
+           get_organization_id(subdomain)      
+        else 
+        end
+      else 
+      end
     end
 
     def get_organization_code(url)
-      #get after "://"
-      url =~ /:\/\//
-      #get before ".kitasang.com"
-      $'=~ /\./
+      url =~ /:\/\//     #get after "://"
+      $'=~ /\.#{DOMAIN}/ #get before ".kitasang.com"
       $` 
     end
-
+   
     def get_organization_id(organization_code)
       Organization.find_by_organization_code(organization_code).id
     end
+ 
+    def url_valid?(url)
+      url.scan(/#{DOMAIN}/).size == 1  
+    end
+
+    def subdomain_valid?(subdomain)
+      subdomain =~ /^[0-9a-z]+$/
+    end
+ 
   end
 end
