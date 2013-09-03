211ea9cb8a5e83482d4c300ee5a6438e4725209b
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-09-03 15:52:59 +0900

    Added relatinship organization and view design

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,9 @@
 class Organization < ActiveRecord::Base
-  attr_accessible :organization_code, :organization_name, :organization_infos, :stores,:stores_attributes, :organization_infos_attributes
+  attr_accessible :organization_code, :organization_name, :organization_infos, :stores,:stores_attributes, :organization_infos_attributes, :view_designs, :view_designs_attributes
   has_many :organization_infos
   has_many :products
   has_many :stores
+  has_many :view_designs
   accepts_nested_attributes_for :stores,
                                 :organization_infos,
                                 :view_designs
--- app/models/view_design.rb
+++ app/models/view_design.rb
@@ -1,4 +1,6 @@
 class ViewDesign < ActiveRecord::Base
   attr_accessible :organization_id, :design_number 
   belongs_to :organization
+ 
+  accepts_nested_attributes_for :organization
 end
