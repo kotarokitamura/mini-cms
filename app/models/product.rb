4ad18fe34af10fd40bf86242c1850141532e09e7
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-09-06 10:31:59 +0900

    Added validation and test in Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :organization_id, :product_name
   belongs_to :organization
+  
+  validates :product_name,
+            :length => {:maximum => 50}
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,36 @@
+# coding: utf-8
 require 'spec_helper'
-
+  
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :products
+
+  before do 
+    @product = Product.new
+  end
+
+  context 'with insert some data' do 
+    it 'should return true when insert under 50 charactors in product_name in English' do
+      product_name = 'a'*50
+      @product.attributes = {:organization_id => 1, :product_name => product_name} 
+      @product.save.should be_true
+    end
+ 
+    it 'should return false when insert over 50 charactors in product_name in English' do
+      product_name = 'a'*51
+      @product.attributes = {:organization_id => 1, :product_name => product_name} 
+      @product.save.should be_false
+    end
+
+    it 'should return false when insert over 50 charactors in product_name in Japanese' do
+      product_name = 'あ'*51
+      @product.attributes = {:organization_id => 1, :product_name => product_name} 
+      @product.save.should be_false
+    end
+
+    it 'should return true when insert under 50 charactors in product_name in Japanese' do
+      product_name = 'あ'*50
+      @product.attributes = {:organization_id => 1, :product_name => product_name} 
+      @product.save.should be_true
+    end
+  end
 end
