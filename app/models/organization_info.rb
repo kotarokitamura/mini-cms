36d9b795d8768c1d517f5c8fc76dd9f96ea3aa22
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-09-06 10:11:02 +0900

    Added validate and test in OrganizationInfo

--- app/models/organization_info.rb
+++ app/models/organization_info.rb
@@ -1,4 +1,13 @@
 class OrganizationInfo < ActiveRecord::Base
   attr_accessible :organization_id, :phone, :product_information_flag, :store_information_flag
   belongs_to :organization
+   
+  validates_format_of :phone,
+                      :with => /\d{2,4}-\d{2,4}-\d{4}/
+
+  validates :product_information_flag,
+            format: { with:/^[0-1]$/}
+
+  validates :store_information_flag,
+            format: { with:/^[0-1]$/}
 end
--- spec/models/organization_info_spec.rb
+++ spec/models/organization_info_spec.rb
@@ -1,5 +1,66 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe OrganizationInfo do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :organizations, :organization_infos
+ 
+  before do
+    @organization_info = OrganizationInfo.new
+  end 
+ 
+  context 'with insert some datas' do
+    it 'should return true insert correct phone' do
+      phone = '000-000-0000'
+      @organization_info.attributes = {:organization_id => 1, :phone => phone}
+      @organization_info.save.should be_true
+    end
+
+    it 'should return false insert blank' do
+      phone = ''
+      @organization_info.attributes = {:organization_id => 1, :phone => phone}
+      @organization_info.save.should be_false
+    end
+
+    it 'should return false insert uncorrect charactors' do
+      phone = 'aaa-aaaa-aaaa'
+      @organization_info.attributes = {:organization_id => 1, :phone => phone}
+      @organization_info.save.should be_false
+    end
+
+    it 'should return true insert correct product_information_flag ' do
+      product_info_falg = 1 
+      @organization_info.attributes = {:organization_id => 1, :phone => '000-000-0000', :product_information_flag => product_info_falg}
+      @organization_info.save.should be_true
+    end
+
+    it 'should return true insert correct product_information_flag ' do
+      product_info_falg = 0 
+      @organization_info.attributes = {:organization_id => 1, :phone => '000-000-0000', :product_information_flag => product_info_falg}
+      @organization_info.save.should be_true
+    end
+
+    it 'should return true insert other of 1 and 0 in product_information_flag' do
+      product_info_falg = 2 
+      @organization_info.attributes = {:organization_id => 1, :phone => '000-000-0000', :product_information_flag => product_info_falg}
+      @organization_info.save.should be_false
+    end
+
+    it 'should return true insert correct store_information_flag ' do
+      store_info_falg = 1 
+      @organization_info.attributes = {:organization_id => 1, :phone => '000-000-0000', :store_information_flag => store_info_falg}
+      @organization_info.save.should be_true
+    end
+
+    it 'should return true insert correct store_information_flag ' do
+      store_info_falg = 0 
+      @organization_info.attributes = {:organization_id => 1, :phone => '000-000-0000', :store_information_flag => store_info_falg}
+      @organization_info.save.should be_true
+    end
+
+    it 'should return true insert other of 1 and 0 in store_information_flag' do
+      store_info_falg = 2 
+      @organization_info.attributes = {:organization_id => 1, :phone => '000-000-0000', :store_information_flag => store_info_falg}
+      @organization_info.save.should be_false
+    end
+  end     
 end
