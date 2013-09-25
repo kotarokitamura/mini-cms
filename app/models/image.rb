0e6048f82f5cb8ed8e159f4c0faaad7c2ca682f9
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-09-25 10:18:12 +0900

    #19: Added validation about image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,8 @@
 class Image < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :logo 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/system/missing/:style/missing.jpg"
+
+  validates_attachment :logo, presence: true,
+    content_type: { content_type: ["image/jpg", "image/png"] },
+    size: { less_than: 2.megabytes }
 end
