a757a56b0299120968aabb8c5cb89882717592c2
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-09-25 10:35:47 +0900

    #19: Added edit and update in image controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,11 @@
 class OrganizationInfosController < ApplicationController
   def edit
     @image = Image.find(paramas[:origanization_id])
+  end
+
+  def update
+    @image = Image.find(params[:organization_id])
+    @image.update_atttributes(params[:image])
+    redirect_to ({:action => 'edit'}), :notice => t('updated_message')
+  end
 end
