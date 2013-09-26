e122453ff5db9ef8d0e16a8b9140e85de374de42
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-09-26 16:48:36 +0900

    #30: Added spec test of view_designs

--- app/models/view_design.rb
+++ app/models/view_design.rb
@@ -1,6 +1,11 @@
 class ViewDesign < ActiveRecord::Base
   attr_accessible :organization_id, :design_number 
   belongs_to :organization
- 
+
   accepts_nested_attributes_for :organization
+
+  validates_numericality_of :design_number,
+                            :only_integer => true,
+                            :greater_than_or_equal_to => 0,
+                            :allow_nil => false
 end
