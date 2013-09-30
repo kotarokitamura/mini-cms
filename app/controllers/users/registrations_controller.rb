cf12cf7cdbc8c673a83be2410b9c8228c778257f
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-09-30 18:26:05 +0900

    #33: Added create user with organization_code

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,22 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def new
-    super
+    @organization = Organization.new
+    @organization.users.build
   end
 
   def create
-    super
+    @organization = Organization.new(params[:organization])
+    ResourceProperty.organization_max_item.times{
+      @organization.stores.build
+      @organization.products.build
+      @organization.organization_infos.build
+    }
+    @organization.view_designs.build
+    @organization.images.build
+    if @organization.save
+      redirect_to @organization
+    else
+      render action: "new"
+    end
   end
 end
