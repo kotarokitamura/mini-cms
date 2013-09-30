ab158e065ef8d70e9d0f423048c6dc18e91dd977
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-09-30 23:46:34 +0900

    #33: Added new routes create next new user

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,12 @@ Minicms::Application.routes.draw do
 
   match "organizations/:organization_id/view_design", :via => :get, :to => "view_designs#edit", :as => "view_designs"
   match "organizations/:organization_id/view_design", :via => :put, :to => "view_designs#update", :as => "view_designs"
-  
-  match "organizations/:organization_id/image", :via => :get, :to => "images#edit" 
+
+  match "organizations/:organization_id/image", :via => :get, :to => "images#edit"
   match "organizations/:organization_id/image", :via => :put, :to => "images#update", :as => "images"
+
+  match "organizations/:organization_id/users", :via => :get, :to => "users/registrations#next_new_user"
+  match "organizations/:organization_id/users", :via => :post, :to => "users/registrations#create_next_new_user"
 
   root :to => "organizations#show"
 end
