e480212d8330ae4d64987c7eb455faeca7b9cab2
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-10-03 15:58:20 +0900

    #31:changed after login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
 
+  def after_sign_in_path_for(resource)
+    organizations_edit_path
+  end
+
   rescue_from Exception, :with => :handle_exceptions unless Rails.application.config.consider_all_requests_local
 
   private
