e2e5f22d80f8623529c9ec76bfbf47fd063e402f
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-10-04 17:30:20 +0900

    #38: fixed homepage controller

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -2,14 +2,12 @@ class HomepagesController < ApplicationController
   skip_before_filter :authenticate_user!, :only => [:show]
   def preview
     @organization = Organization.find(current_user.organization_id)
-    @organization_infos = @organization.organization_infos
-    @stores = @organization.stores
-    render :action => 'show'
+    render :template => "homepages/show#{@organization.view_designs.first.design_number}.html.haml", :layout => false
   end
 
   def show
     organization_id = Homepage.get_organization_id_by_subdomain(request.url) or InvalidUrlError
     @organization = Organization.find(organization_id)
-    render :template => "homepages/show#{@organization.view_designs.first.design_number}.html.haml"
+    render :template => "homepages/show#{@organization.view_designs.first.design_number}.html.haml", :layout => false
   end
 end
