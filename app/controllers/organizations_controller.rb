256de7f7466b9f6ab1c61520a5a095b7cb5dc9a2
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-10-04 19:35:59 +0900

    #38: Added flash massage to all templat

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -6,9 +6,11 @@ class OrganizationsController < ApplicationController
   def update
     @organization = Organization.find(current_user.organization_id)
     if @organization.update_attributes(params[:organization])
-      render :action => 'edit', notice: t('action.updated_message')
+      flash[:notice] = t('action.updated_message')
+      render :action => 'edit'
     else
-      render :action => 'edit', notice: t('action._failed_updated_message')
-    end
+      flash[:notice] = t('action._failed_updated_message')
+      render :action => 'edit'
+   end
   end
 end
