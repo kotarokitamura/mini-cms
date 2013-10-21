8e9bdb048236ea15db974392f1670500602b4bd7
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-10-21 20:57:00 +0900

    #46: Added access_filter in registrations_controller

--- app/controllers/view_designs_controller.rb
+++ app/controllers/view_designs_controller.rb
@@ -1,10 +1,12 @@
 class ViewDesignsController < ApplicationController
   def edit
     @organization = Organization.find(get_organization_id_by_current_user)
+    raise ForbiddenError unless @organization.access_filter?(current_user)
   end
 
   def update
     @organization = Organization.find(get_organization_id_by_current_user)
+    raise ForbiddenError unless @organization.access_filter?(current_user)
     if @organization.update_attributes(params[:organization])
       flash[:notice] = t('action.updated_message')
       render :action => 'edit'
