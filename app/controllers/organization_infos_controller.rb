9e60f707a0141fc747b0dc5bf286c303d70f21a2
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-11-05 18:54:29 +0900

    #50: Comment out all unuse method

--- app/controllers/organization_infos_controller.rb
+++ app/controllers/organization_infos_controller.rb
@@ -1,18 +1,18 @@
 class OrganizationInfosController < ApplicationController
-  def edit
-    @organization = Organization.find(params[:id])
-    raise ForbiddenError unless @organization.access_filter?(current_user)
-  end
+  #def edit
+  #  @organization = Organization.find(params[:id])
+  #  raise ForbiddenError unless @organization.access_filter?(current_user)
+  #end
 
-  def update
-    @organization = Organization.find(params[:id])
-    raise ForbiddenError unless @organization.access_filter?(current_user)
-    if @organization.update_attributes(params[:organization])
-      flash[:notice] = t('action.updated_message')
-      render :action => 'edit'
-    else
-      flash[:notice] = t('action.failed_update_message')
-      render :action => 'edit'
-    end
-  end
+  #def update
+  #  @organization = Organization.find(params[:id])
+  #  raise ForbiddenError unless @organization.access_filter?(current_user)
+  #  if @organization.update_attributes(params[:organization])
+  #    flash[:notice] = t('action.updated_message')
+  #    render :action => 'edit'
+  #  else
+  #    flash[:notice] = t('action.failed_update_message')
+  #    render :action => 'edit'
+  #  end
+  #end
 end
