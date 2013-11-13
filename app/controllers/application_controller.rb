47472a54b908c19f375b80555ddc144c22759879
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-11-13 10:40:35 +0900

    #51: Added ForbiddenError page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,6 +36,9 @@ class ApplicationController < ActionController::Base
     when InvalidUrlError, ActiveRecord::RecordNotFound, ActionController::RoutingError, ActionController::UnknownAction
       render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
       logger.info("Rendering 404 with exception: #{e.message}")
+    when ForbiiddenError
+      render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+      logger.info("Rendering 403 with exception: #{e.message}")
     else
       render :file => "#{Rails.root}/public/500.html", :status => 500, :layout => false
       logger.info("Rendering 505 with exception: #{e.message}")
