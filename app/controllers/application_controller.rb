5a02a9533af0dcdef5a73dad02e37ed131ff1a44
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-11-13 10:43:28 +0900

    Fix spell

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < ActionController::Base
     when InvalidUrlError, ActiveRecord::RecordNotFound, ActionController::RoutingError, ActionController::UnknownAction
       render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
       logger.info("Rendering 404 with exception: #{e.message}")
-    when ForbiiddenError
+    when ForbiddenError
       render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
       logger.info("Rendering 403 with exception: #{e.message}")
     else
